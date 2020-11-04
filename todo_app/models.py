e1c676f7e5ec579031003bec08cb9a35da18b724
Iglek Kuat <dizinnes228@gmail.com>
2020-11-05 03:07:14 +0600

    feat: create trial models

--- todo_app/models.py
+++ todo_app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Executor(models.Model):
+    name = models.CharField(max_length=32, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=100, null=True)
+    executor = models.ForeignKey(Executor, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=32, null=True)
+    tasks = models.ManyToManyField(Task)
+
+    def __str__(self):
+        return self.name
