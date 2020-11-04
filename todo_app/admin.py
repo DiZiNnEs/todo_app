404ff734f2bbd4f47738d3279a54f18495c6d3f2
Iglek Kuat <dizinnes228@gmail.com>
2020-11-05 03:07:29 +0600

    feat: add models to django admin

--- todo_app/admin.py
+++ todo_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import (
+    Project,
+    Task,
+    Executor,
+)
 
-# Register your models here.
+admin.site.register(Project)
+admin.site.register(Task)
+admin.site.register(Executor)
