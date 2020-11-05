5b004e0f795e7824e264eb4805d5d1ad4579d322
Iglek Kuat <dizinnes228@gmail.com>
2020-11-05 17:12:17 +0600

    feat: add display of project and their number

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 
+from django.core.handlers.wsgi import WSGIRequest
 
-def home(request):
-    return render(request, 'todo_app/index.html')
+from .models import Project
+
+
+def home(request: WSGIRequest) -> render:
+    project_list = Project.objects.all()
+    number_tasks_project = Project.objects.count()
+
+    context = {
+        'project_list': project_list,
+        'number_of_tasks_in_project': number_tasks_project,
+    }
+
+    return render(request, 'todo_app/index.html', context)
