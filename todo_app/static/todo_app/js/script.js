f9981de1fa8d4aa008d37c50001d328406054df7
Iglek Kuat <dizinnes228@gmail.com>
2020-11-26 22:34:12 +0600

    feat: add delete project trough http request

--- todo_app/static/todo_app/js/script.js
+++ todo_app/static/todo_app/js/script.js
@@ -1,9 +1,21 @@
-const app = Vue.createApp({
-    data() {
-        return {
-            outputHello: 'Hello World!',
-        };
+const App = {
+  data() {
+    return {};
+  },
+  methods: {
+    getUserAgreementOnDeleteProject(id) {
+      const questionToUserAboutDeleteProject = confirm('Are you want to delete project ?');
+      questionToUserAboutDeleteProject ? this.deleteProjectThroughApi(id) && location.reload() : console.log('Project not deleted');
     },
-});
+    deleteProjectThroughApi(id) {
+      axios
+        .delete(`http://127.0.0.1:8000/project-delete/${id}/`, {
+          headers: {'X-CSRFToken': '0hekCQGnD4qwDkXxrQdMRCNQRyyoPW0fhxoGuttRaf1BQEbZBNEWQzPhjWEMKVLe'}
+        })
+        .then(response => (this.resultAboutDelete = response))
+        .catch(error => console.log(error));
+    },
+  },
+};
 
-app.mount('#events');
+Vue.createApp(App).mount('#events');
